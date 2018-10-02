da468e6e293fb7309c029fe29c5555ddf0b93fdf
Rob Harrison <robwithhair@gmail.com>
2018-10-02 19:37:52 +0100

    Added packages

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "CraftOS",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(name: "craft", targets: ["Craft"]),
         .library(
             name: "CraftOS",
             targets: ["CraftOS"]),
@@ -14,10 +15,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Craft",
+            dependencies: [ "CraftOS", "Utility" ]
+        ),
         .target(
             name: "CraftOS",
             dependencies: []),
